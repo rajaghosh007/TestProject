3f46430ef033c41c2776e7421195ab4cf3d202c3
rajaghosh007 <48590462+rajaghosh007@users.noreply.github.com>
2020-07-07 13:39:06 +0530

    Create mygoservice.go

---
+++ mygoservice.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	//	"mime"
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	message := "This is a test call"
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(message))
+}
+func setupRoutes() {
+	http.HandleFunc("/upload", uploadFile)
+	http.ListenAndServe(":8181", nil)
+}
+
+func main() {
+	fmt.Println("Hello World")
+	setupRoutes()
+}
+
+func renderError(w http.ResponseWriter, message string, statusCode int) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(message))
+}
